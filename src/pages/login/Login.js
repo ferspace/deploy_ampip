30405282c0001d705e89de62ed775704aeee40d2
ferpharos <fernandovg@pharospayments.comm>
2021-08-09 12:57:26 -0500

    login

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -16,7 +16,7 @@ import classnames from "classnames";
 import useStyles from "./styles";
 
 // logo
-import logo from "./logo.svg";
+import logo from "./ampip.png";
 import google from "../../images/google.svg";
 
 // context
@@ -35,8 +35,8 @@ function Login(props) {
   var [error, setError] = useState(null);
   var [activeTabId, setActiveTabId] = useState(0);
   var [nameValue, setNameValue] = useState("");
-  var [loginValue, setLoginValue] = useState("admin@flatlogic.com");
-  var [passwordValue, setPasswordValue] = useState("password");
+  var [loginValue, setLoginValue] = useState("");
+  var [passwordValue, setPasswordValue] = useState("");
 
   //usuario y contraseña
   const [user, setUser] = useState({email:"",});
@@ -81,8 +81,7 @@ function Login(props) {
   return (
     <Grid container className={classes.container}>
       <div className={classes.logotypeContainer}>
-        <img src={logo} alt="logo" className={classes.logotypeImage} />
-        <Typography className={classes.logotypeText}>Material Admin</Typography>
+        <Typography className={classes.logotypeText}>imagen inspiracion</Typography>
       </div>
       <div className={classes.formContainer}>
         <div className={classes.form}>
@@ -94,22 +93,14 @@ function Login(props) {
             centered
           >
             <Tab label="Login" classes={{ root: classes.tab }} />
-            <Tab label="New User" classes={{ root: classes.tab }} />
-          </Tabs>
+{/*             <Tab label="New User" classes={{ root: classes.tab }} />
+ */}          </Tabs>
           {activeTabId === 0 && (
             <React.Fragment>
               <Typography variant="h1" className={classes.greeting}>
-                Good Morning, User
+                <img src={logo} alt="logo" className={classes.logotypeImage} />
               </Typography>
-              <Button size="large" className={classes.googleButton}>
-                <img src={google} alt="google" className={classes.googleIcon} />
-                &nbsp;Sign in with Google
-              </Button>
-              <div className={classes.formDividerContainer}>
-                <div className={classes.formDivider} />
-                <Typography className={classes.formDividerWord}>or</Typography>
-                <div className={classes.formDivider} />
-              </div>
+            
               <Fade in={error}>
                 <Typography color="secondary" className={classes.errorMessage}>
                   Something is wrong with your login or password :(
--- src/pages/maps/Maps.js
+++ src/pages/maps/Maps.js
@@ -1,7 +1,5 @@
 import React, { useState, useEffect } from "react";
 import {
-  Typography,
-  Grid,
   Tabs,
   Tab,
   Paper,
