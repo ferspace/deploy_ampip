dd3be41e255d8dcb850ec711ce4e02f2136c3276
edyMartin2 <evalerio.sdc@hotmail.com>
2021-08-14 11:25:00 -0500

    ok

--- src/pages/Parques/SpecificForm/index.js
+++ src/pages/Parques/SpecificForm/index.js
@@ -19,8 +19,13 @@ const BasicMap = withScriptjs(
         lat: parseFloat(-37.813179),
         lng: parseFloat(144.950259),
       }}
+
+      onClick={ev => {
+        console.log("latitide = ", ev.latLng.lat());
+        console.log("longitude = ", ev.latLng.lng());
+      }}
     >
-      <Marker position={{ lat: -37.813179, lng: 144.950259 }} />
+      <Marker position={{ lat: -37.813179, lng: 144.950259 }} onClick={(e) => { console.log(e) }} />
     </GoogleMap>
   )),
 );
@@ -78,8 +83,8 @@ const SpecificForm = (props) => {
               "industry": values.user.industry,
               "superficie": values.user.superficie,
               "suprficie_total": values.user.suprficie_total,
-              "superficie_urbanizada": values.user.superficie_urbanizada ,
-              "superficie_disponible": values.user.superficie_disponible ,
+              "superficie_urbanizada": values.user.superficie_urbanizada,
+              "superficie_disponible": values.user.superficie_disponible,
               "inicio_de_operaciones": values.user.inicio_de_operaciones,
               "number_employe": values.user.number_employe,
               "practices_recognition": values.user.practices_recognition,
@@ -144,7 +149,7 @@ const SpecificForm = (props) => {
   }, []);
 
   return (
-    <Form {...layout} name="nest-messages" onFinish={onFinish} validateMessages={validateMessages} style={{height:"1700px"}}>
+    <Form {...layout} name="nest-messages" onFinish={onFinish} validateMessages={validateMessages} style={{ height: "1700px" }}>
       <Form.Item name={["user", "corpoate_id"]} label="Corporativos" rules={[{ required: true }]}>
         <Select
           placeholder="Select a option and change input text above"
@@ -193,42 +198,42 @@ const SpecificForm = (props) => {
         <Input />
       </Form.Item>
       <Form.Item name={['user', 'infrastructure']} label="Infraestructura Disponible" rules={[{ required: true }]}>
-      <Select
-            placeholder="Select a option and change input text above"
-            allowClear
-            mode="multiple"
-          >
-            <Option value="Al menos 0.5 litros agua por segundo por ha">Al menos 0.5 litros agua por segundo por ha</Option>
-            <Option value="Pavimento">Pavimento</Option>
-            <Option value="Banquetas">Banquetas</Option>
-            <Option value="Drenaje Sanitario">Drenaje Sanitario</Option>
-            <Option value="Drenaje Pluvial">Drenaje Pluvial</Option>
-            <Option value="Planta de tratamiento de Agua">Planta de tratamiento de Agua</Option>
-            <Option value="Gas Natural">Gas Natural</Option>
-            <Option value="Alumbrado público">Alumbrado público</Option>
-            <Option value="Instalación eléctrica">Instalación eléctrica</Option>
-            <Option value="Subestación eléctrica">Subestación eléctrica</Option>
-            <Option value="Telefonía">Telefonía</Option>
-            <Option value="Comunicación Satelital">Comunicación Satelital</Option>
-            <Option value="Instalación Digital">Instalación Digital</Option>
-            <Option value="Espuela de Ferrocarril">Espuela de Ferrocarril</Option>
-            <Option value="Estación de bomberos">Estación de bomberos</Option>
-            <Option value="Áreas verdes o recreativas">Áreas verdes o recreativas</Option>
-            <Option value="Guardería">Guardería</Option>
-            <Option value="Centro de Capacitación">Centro de Capacitación</Option>
-            <Option value="Seguridad">Seguridad</Option>
-            <Option value="Transporte interno de personal">Transporte interno de personal</Option>
-            <Option value="Transporte Urbano">Transporte Urbano</Option>
-            <Option value="Recolección de basura">Recolección de basura</Option>
-            <Option value="Aduana interna">Aduana interna</Option>
-            <Option value="Agente aduanal">Agente aduanal</Option>
-            <Option value="Servicios de consultoria">Servicios de consultoria</Option>
-            <Option value="Programa shelter">Programa shelter</Option>
-            <Option value="Servicio Built to suit">Servicio Built to suit</Option>
-            <Option value="Reglamento interno">Reglamento interno</Option>
-            <Option value="Oficinas administrativas">Oficinas administrativas</Option>
-            <Option value="Otros">Otros</Option>
-          </Select>
+        <Select
+          placeholder="Select a option and change input text above"
+          allowClear
+          mode="multiple"
+        >
+          <Option value="Al menos 0.5 litros agua por segundo por ha">Al menos 0.5 litros agua por segundo por ha</Option>
+          <Option value="Pavimento">Pavimento</Option>
+          <Option value="Banquetas">Banquetas</Option>
+          <Option value="Drenaje Sanitario">Drenaje Sanitario</Option>
+          <Option value="Drenaje Pluvial">Drenaje Pluvial</Option>
+          <Option value="Planta de tratamiento de Agua">Planta de tratamiento de Agua</Option>
+          <Option value="Gas Natural">Gas Natural</Option>
+          <Option value="Alumbrado público">Alumbrado público</Option>
+          <Option value="Instalación eléctrica">Instalación eléctrica</Option>
+          <Option value="Subestación eléctrica">Subestación eléctrica</Option>
+          <Option value="Telefonía">Telefonía</Option>
+          <Option value="Comunicación Satelital">Comunicación Satelital</Option>
+          <Option value="Instalación Digital">Instalación Digital</Option>
+          <Option value="Espuela de Ferrocarril">Espuela de Ferrocarril</Option>
+          <Option value="Estación de bomberos">Estación de bomberos</Option>
+          <Option value="Áreas verdes o recreativas">Áreas verdes o recreativas</Option>
+          <Option value="Guardería">Guardería</Option>
+          <Option value="Centro de Capacitación">Centro de Capacitación</Option>
+          <Option value="Seguridad">Seguridad</Option>
+          <Option value="Transporte interno de personal">Transporte interno de personal</Option>
+          <Option value="Transporte Urbano">Transporte Urbano</Option>
+          <Option value="Recolección de basura">Recolección de basura</Option>
+          <Option value="Aduana interna">Aduana interna</Option>
+          <Option value="Agente aduanal">Agente aduanal</Option>
+          <Option value="Servicios de consultoria">Servicios de consultoria</Option>
+          <Option value="Programa shelter">Programa shelter</Option>
+          <Option value="Servicio Built to suit">Servicio Built to suit</Option>
+          <Option value="Reglamento interno">Reglamento interno</Option>
+          <Option value="Oficinas administrativas">Oficinas administrativas</Option>
+          <Option value="Otros">Otros</Option>
+        </Select>
       </Form.Item>
       <Form.Item name={['user', 'inicio_de_operaciones']} label="Inicio de Operaciones" rules={[{ required: true }]}>
         <Input />
@@ -240,22 +245,22 @@ const SpecificForm = (props) => {
         <Input />
       </Form.Item>
       <Form.Item name={['user', 'practices_recognition']} label="Reconocimientos mejores Prácticas" rules={[{ required: true }]}>
-      <Select
-            placeholder="Select a option and change input text above"
-            allowClear
-            mode="multiple"
-          >
-            <Option value="Norma Mexiana de Parque Industrial">Norma Mexiana de Parque Industrial</Option>
-            <Option value="Parque Industrial Verde">Parque Industrial Verde</Option>
-            <Option value="Calidad Ambiental (PROFEPA)">Calidad Ambiental (PROFEPA)</Option>
-            <Option value="Parque Industrial Sustentable">Parque Industrial Sustentable</Option>
-            <Option value="Parque Industrial Limpio">Parque Industrial Limpio</Option>
-            <Option value="Parque Industrial Seguro">Parque Industrial Seguro</Option>
-            <Option value="OEA">OEA</Option>
-          </Select>
+        <Select
+          placeholder="Select a option and change input text above"
+          allowClear
+          mode="multiple"
+        >
+          <Option value="Norma Mexiana de Parque Industrial">Norma Mexiana de Parque Industrial</Option>
+          <Option value="Parque Industrial Verde">Parque Industrial Verde</Option>
+          <Option value="Calidad Ambiental (PROFEPA)">Calidad Ambiental (PROFEPA)</Option>
+          <Option value="Parque Industrial Sustentable">Parque Industrial Sustentable</Option>
+          <Option value="Parque Industrial Limpio">Parque Industrial Limpio</Option>
+          <Option value="Parque Industrial Seguro">Parque Industrial Seguro</Option>
+          <Option value="OEA">OEA</Option>
+        </Select>
       </Form.Item>
       <Form.Item name={['user', 'superficie']} label="Superficie" rules={[{ required: true }]}>
-        <Input/>
+        <Input />
       </Form.Item>
 
       <Form.Item name={['user', 'superficie_total']} label="Superficie Total" rules={[{ required: true }]}>
@@ -281,17 +286,17 @@ const SpecificForm = (props) => {
         </Select>
       </Form.Item>
       <Form.Item>
-      <BasicMap
-        googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyCFdQ7O0MIewEqbyXhW0k9XemMqnYx0aDQ"
-        loadingElement={<div style={{ width: "inherit" }} />}
-        containerElement={<div style={{ height: "15em" }} />}
-        mapElement={<div style={{ height: "100%" }} />}
-      />
+        <BasicMap
+          googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyCFdQ7O0MIewEqbyXhW0k9XemMqnYx0aDQ"
+          loadingElement={<div style={{ width: "inherit" }} />}
+          containerElement={<div style={{ height: "15em" }} />}
+          mapElement={<div style={{ height: "100%" }} />}
+        />
       </Form.Item>
       <Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 8 }}>
         <Button style={{ backgroundColor: "#00afb7", borderColor: "#00afb7", color: "#ffffff" }} type="primary" htmlType="submit">
           Enviar
-        </Button> 
+        </Button>
       </Form.Item>
     </Form>
   )
