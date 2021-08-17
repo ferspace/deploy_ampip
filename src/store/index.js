705f2f6e79ebddc52dc78d6abe3cbcb4cab22780
edyMartin2 <evalerio.sdc@hotmail.com>
2021-08-17 12:43:43 -0500

    ok

--- src/store/index.js
+++ src/store/index.js
@@ -4,7 +4,7 @@ const store = {
     //https://ampip-back-33cr9.ondigitalocean.app/api/v1/sign_in
     //https://ampip-back-33cr9.ondigitalocean.app/api/v1/sign_in
     //https://cepomex.herokuapp.com/api/v1/zip_codes?zip_code=529
-    URL_PRODUCTION: 'https://ampip-back-44yib.ondigitalocean.app/api/v1',
+    URL_PRODUCTION: 'http://localhost:3001/api/v1',
     ADDRESS: 'https://cepomex.herokuapp.com/api/v1'
 
 }
