6c062b5cd5daab3eb4114894cc6ca5f7dd19bb35
edyMartin2 <evalerio.sdc@hotmail.com>
2021-08-19 11:06:20 -0500

    Master nuevo

--- src/pages/Arrendatario/SpecificForm/index.js
+++ src/pages/Arrendatario/SpecificForm/index.js
@@ -33,7 +33,11 @@ const SpecificForm = (props) => {
           'Content-Type': 'application/json'
         },
       }).then((response) => {
-        setCorporates(response.data.message.rescueParks)
+       var naves = response.data.message.allProperties.naves;
+       var terrenos = response.data.message.allProperties.terrenos;
+       var navy = response.data.message.allProperties.ter;
+       var ter = response.data.message.allProperties.ter;
+        setCorporates(...naves,terrenos, navy, ter);
         // setCorporates(response.data)
         //setPost(response.data);
       });
